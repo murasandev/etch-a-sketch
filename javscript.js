171554b2553b3f346534402f0972499adc63b71e
Dylan <dmurayama93@gmail.com>
2023-10-25 20:11:24 -1000

    remove javascript.jss

---
+++ javscript.js
@@ -0,0 +1,8 @@
+const square = document.querySelector(".square");
+const getSquareColor = document.createAttribute(".square");
+
+square.addEventListener("mouseover", () => {
+    // change color
+    console.log("hover");
+    
+});
