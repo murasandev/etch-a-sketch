d777fdbdbdd768cf74e5229ee46d2068fd780565
Dylan <dmurayama93@gmail.com>
2023-10-25 20:29:58 -1000

    add mouseover change color effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-const square = document.querySelector(".square");
-const getSquareColor = document.createAttribute(".square");
-
-square.addEventListener("mouseover", () => {
-    // change color
+let squareColor = document.querySelector(".square");
+let isInitialColor = true;
+squareColor.addEventListener("mouseover", () => {
+    // change color on hover
     console.log("hover");
-    
+    if (isInitialColor) {
+        squareColor.style.backgroundColor = "white";
+        isInitialColor = false;
+    }
+    else {
+        squareColor.style.backgroundColor = "black";
+        isInitialColor = true;
+    }
 });
